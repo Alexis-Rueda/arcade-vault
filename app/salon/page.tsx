b5aa110b6dc4a7a34d11e51c96b1f5f0e4ccdce2
alexis.rueda <alexisrueda34@gmail.com>
2026-07-21 14:08:36 -0600

    feat(pages): agregar página SalonPage y componente HallOfFameScreen con Podium

---
+++ app/salon/page.tsx
@@ -0,0 +1,11 @@
+import { Nav } from "@/components/Nav";
+import { HallOfFameScreen } from "@/components/HallOfFameScreen";
+
+export default function SalonPage() {
+  return (
+    <>
+      <Nav />
+      <HallOfFameScreen />
+    </>
+  );
+}
